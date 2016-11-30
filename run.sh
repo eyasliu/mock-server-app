f600d85e870e5cf01d2a0134385eff23dc749b48
liuyuesong <liuyuesong@kingsoft.com>
2016-11-30 16:45:01 +0800

    start shell

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-npm i --production
-mkdir -p ./db
-forever start server.js
+if [ `which forever` ]; then
+    echo "forever is ok!"
+else
+    npm i -g forever
+fi
+
+if [ $1 == "stop" ]; then
+    forever stop server.js
+else
+    echo "install production dependencies"
+    npm i --production
+    echo "install dependencies OK!"
+    mkdir -p ./db
+    echo "starting service"
+    forever start server.js
+    echo "start service OK"
+fi
