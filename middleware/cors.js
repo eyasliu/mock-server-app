f4a55b9e0400e10bc1dc539a93b748101a0c8155
liuyuesong <liuyuesong@kingsoft.com>
2016-11-30 11:43:35 +0800

    fix cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ export default domain => async (context, next) => {
     context.set('Access-Control-Allow-Credentials', 'true');
     context.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     context.set('Access-Control-Allow-Headers', 'Accept,Content-Type');
-    context.set('Access-Control-Allow-Origin', `${context.protocol}://${context.host}`);
+    context.set('Access-Control-Allow-Origin', context.req.headers.origin || `${context.protocol}://${context.host}`);
     context.set('Access-Control-Expose-Headers', 'Accept,Content-Type');
     await next()
 }
